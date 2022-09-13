f7b7483ed3d179c90dee50bf7d34c670627a3f94
he1gi <oleh.mishchenko1@gmail.com>
2022-09-13 20:22:36 +0300

    bugfix

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 import { Message } from './Notification.styled';
 
-export default Notification = ({ message }) => {
+function Notification({ message }) {
   return <Message>{message}</Message>;
-};
+}
 
 Notification.propTypes = {
   message: PropTypes.string.isRequired,
 };
+export default Notification;
